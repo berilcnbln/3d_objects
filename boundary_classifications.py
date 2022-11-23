6535285d997f20195b52659a85cd6050b2d4d06c
berilcnbln <49610804+berilcnbln@users.noreply.github.com>
2022-11-23 14:40:18 +0300

    Update boundary_classifications.py

--- boundary_classifications.py
+++ boundary_classifications.py
@@ -7,9 +7,9 @@ import math
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-
-
-
+import time
+import itertools
+from numpy.linalg import norm
 
 SOURCE_PCD =  o3d.io.read_triangle_mesh(r"/Users/beril/PycharmProjects/pythonProject2/files/3.ply")
 pcd = SOURCE_PCD.sample_points_poisson_disk(number_of_points = 10000)
@@ -31,7 +31,6 @@ colors = plt.get_cmap("tab20")(labels / (max_label
 if max_label > 0 else 1))
 colors[labels < 0] = 0
 pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
-#o3d.visualization.draw_geometries([pcd])
 segment_models={}
 segments={}
 max_plane_idx=10
@@ -58,6 +57,7 @@ for i in range(max_plane_idx):
 
 pcl += rest
 
+#colors
 for i in cl:
     print(i)
 
@@ -75,18 +75,31 @@ distances = np.max(pcl.compute_nearest_neighbor_distance())
 distances2 = np.min(pcl.compute_nearest_neighbor_distance())
 blacks = []
 
-#holds data of each black points neighbours
+#holds data of each black points' neighbours
 dic = []
-#check all black points until stack is empty
+
+print("rest length" + str(len(np.asarray(rest.points))))
+
+
+hull, _ = rest.compute_convex_hull()
+hull_ls = o3d.geometry.LineSet.create_from_triangle_mesh(hull)
+box = hull_ls.get_axis_aligned_bounding_box()
+v_ = box.volume()
+
+
+v_  = round(v_,0) 
+print("volume " + str((v_ )))
+
+start_time = time.time()
+
 for pointt in np.asarray(rest.points):
-    [k, idx, _] = pcd_tree.search_radius_vector_3d(pointt, distances)
+    [k, idx, _] = pcd_tree.search_radius_vector_3d(pointt, distances + (v_ /1000000))
     #[k, idx, _] = pcd_tree.search_knn_vector_3d(pointt, 4)
-    #check black point's neighbours
     arr = [0,0,0,0,0,0,0,0]
     n = 0
     for i in np.asarray(pcl.points)[idx[1:], :]:
-        #check which segments are around the current black point 
         
+        #check which segments are around the current black point 
         if (i in np.asarray(segments[0].points)) or  (i in np.asarray(segments[1].points)) :
             print("segment0")
             arr[0] = 1
@@ -107,6 +120,7 @@ for pointt in np.asarray(rest.points):
             print("segment4")
             arr[4] = 1
             n = 1
+            
     print("---------------------------------------------")
     #completed the process for one black point
 
@@ -122,75 +136,136 @@ for pointt in np.asarray(rest.points):
 
 
 arr_ = []
-#compare each black points with other black point
-
 visited = [False] * len(dic)
+n = 0
 
-
+print("dic lenght" + str(len(dic)))
+#her siyah noktanın komşularıyla olan bilgileri 
 #for i in dic:
 #    print(i)
-#her siyah noktanın komşularıyla olan bilgileri var
+
+x =0
 for i in range(0,len(dic)):
     point_ = []
-    #bakmadıysan bak
     if visited[i]== False:
         visited[i] = True
         n = 0
         point_.append([dic[i][5],dic[i][6],dic[i][7]])
+        x += 1
         for a in range(i+1,len(dic)):
             if visited[a]== False:
+                #if (abs(dic[i][0]-dic[a][0])<1) & (abs(dic[i][1]-dic[a][1]) < 1)& (abs(dic[i][2]-dic[a][2]) < 1)& (abs(dic[i][3]-dic[a][3]) < 1) & (abs(dic[i][4]-dic[a][4])< 1):
                 if (dic[i][0]==dic[a][0]) & (dic[i][1]==dic[a][1]) & (dic[i][2]==dic[a][2]) & (dic[i][3]==dic[a][3]) & (dic[i][4]==dic[a][4]):
                     point_.append([dic[a][5],dic[a][6],dic[a][7]])
                     visited[a] = True
                     n = 1
-
+                    x += 1
         #holds black points have same neighbours
         if n==1:
             arr_.append(point_)
-     
-                            
+            
+print("x" + str(x))    
+colored_time = time.time() - start_time
+
 x = 0
 y = 0
+#point index in cluster
+c_index = []
+n_pointclouds= []
 rest.paint_uniform_color([1, 1, 1])
 for i in arr_: 
     # a is black point
-    if len(i)<70:
+    if len(i)< 20:
         continue
+    c_i = []
+    pcl = o3d.geometry.PointCloud()
+    pcl.points = o3d.utility.Vector3dVector(np.random.randn(len(i),3))
+    b = 0
     for a in i:
         rest.points[x] = a
+        pcl.points[b] = a
+        b += 1
+        c_i.append(x)
         rest.colors[x] = cl[y]
         x += 1
     #print(cll[y])
+    n_pointclouds.append(pcl)
+    c_index.append(c_i)
     y +=1
     if y == 10:
         y=0
 
-o3d.visualization.draw_geometries([rest])        
+o3d.visualization.draw_geometries([rest])      
+print("kaç cluster var ")
+print(len(n_pointclouds))
+
+
+for i in np.asarray(n_pointclouds[3].points):
+    print("one point cloud")
+    print(i)
+
+   
+    
+    
+rest.normals = o3d.utility.Vector3dVector(np.zeros((1, 3)))
+rest.estimate_normals()
+rest.orient_normals_consistent_tangent_plane(100)
+rest.normalize_normals()
+
+ind = np.where(np.asarray(rest.points) == n_pointclouds[2].points[7])[0][0]
+
+ind2 = np.where(np.asarray(rest.points) == n_pointclouds[2].points[5])[0][0]
+
+cosine = np.dot(rest.normals[ind],rest.normals[ind2])/(norm(rest.normals[ind])* norm(rest.normals[ind2]))
+
+print("cosine:")
+print(round(cosine,0))
+
+o3d.visualization.draw_geometries([n_pointclouds[2]])   
+lst = list(itertools.product(n_pointclouds[2].points, repeat=2))
+for i in lst:
+    ind = np.where(np.asarray(rest.points) == i[0])[0][0]
+    ind2 = np.where(np.asarray(rest.points) == i[1])[0][0]
+    cosine = np.dot(rest.normals[ind],rest.normals[ind2])/(norm(rest.normals[ind])*norm(rest.normals[ind2]))
+    #trashold ekle yuvarla 
+    if round(cosine,0)!=1:
+        print(round(cosine,0))
+        print(str(ind) + str("  ") + str(ind2))
         
+       
+
+
+
+    
+start_time = time.time()
 x = []
 for i in range(0, len(np.asarray(rest.points))):
     if ((np.asarray(rest.colors)[i])[0]==[1]) & ((np.asarray(rest.colors)[i])[1]==[1]) & ((np.asarray(rest.colors)[i])[2]==[1]):
         x.append(rest.points[i])
 print(len(x))
-
+finding_uncolored_colors = time.time() - start_time
 
 
 pcd_tree = o3d.geometry.KDTreeFlann(pcn)
-distances = np.max(pcn.compute_nearest_neighbor_distance())
+distances = np.min(pcn.compute_nearest_neighbor_distance())
 visited = [False] * len(x)
+
+
+
+
+
+start_time = time.time()
 a = 0
 while a!=(len(x)):
     for i in range(0, len(x)):
         if visited[i]==False:
-            [k, idx, _] = pcd_tree.search_radius_vector_3d(x[i], distances* (i+10))
+            [k, idx, _] = pcd_tree.search_radius_vector_3d(x[i], distances)
             #[k, idx, _] = pcd_tree.search_knn_vector_3d(blacks[i], 4)
-            
             #check black point's neighbours
             n =0
             if len(np.asarray(pcn.points)[idx[1:], :])==0:
                 a+=1
             for i_ in np.asarray(pcn.points)[idx[1:], :]:
-                
                 for b in range(0, len(arr_)):
                     if n==0:
                         #komşusu renkli mi 
@@ -201,18 +276,24 @@ while a!=(len(x)):
                             n=1
                             print(x[i])
                         
-               
+colored_black_points = time.time() - start_time               
  
 print("finish")
-                           
-x = 0
-y = 0
+ 
+
+    
+ 
 
 pcl = o3d.geometry.PointCloud()
 pcl.points = o3d.utility.Vector3dVector(np.random.randn(10000,3))
+x = 0
+y = 0
+
 pcl.paint_uniform_color([1, 1, 1])
 for i in arr_: 
     # a is black point
+    if len(i)< 20:
+        continue    
     for a in i:
         pcl.points[x] = a
         pcl.colors[x] = cl[y]
@@ -222,7 +303,8 @@ for i in arr_:
     if y == 10:
         y=0
 
-o3d.visualization.draw_geometries([pcl])        
+
+o3d.visualization.draw_geometries([pcl])
 
 
 
